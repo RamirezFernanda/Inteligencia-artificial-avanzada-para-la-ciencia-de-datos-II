9ccc985985b2995e7e96429247afbaa179a90d22
RamirezFernanda <ramirez.bmfernanda@gmail.com>
2022-11-02 23:56:37 -0600

    Update CNNKeras

---
+++ Modulo2/CNN_Keras/CNNKeras.py
@@ -0,0 +1,100 @@
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+from tensorflow import keras
+from keras import layers
+import pathlib
+import matplotlib.pyplot as plt
+
+
+original_dir = pathlib.Path("train")
+new_base_dir = pathlib.Path("horse_or_human")
+
+
+train_dataset = image_dataset_from_directory(
+    new_base_dir / "train",
+    image_size=(180, 180),
+    batch_size=32)
+validation_dataset = image_dataset_from_directory(
+    new_base_dir / "validation",
+    image_size=(180, 180),
+    batch_size=32)
+
+# Test
+test_dataset = image_dataset_from_directory(
+    new_base_dir / "test",
+    image_size=(180, 180),
+    batch_size=32)
+
+for data_batch, labels_batch in train_dataset:
+    print("data batch shape:", data_batch.shape)
+    print("labels batch shape:", labels_batch.shape)
+    break
+
+data_augmentation = keras.Sequential([layers.RandomFlip("horizontal"),
+                                      layers.RandomRotation(0.1),
+                                      layers.RandomZoom(0.2)])
+
+
+model = keras.Sequential([keras.Input(shape=(180, 180, 3)),
+                          layers.RandomFlip("horizontal"),
+                          layers.RandomRotation(0.1),
+                          layers.RandomZoom(0.2),
+                          layers.Rescaling(1./255),
+                          layers.Conv2D(filters=32, kernel_size=3,
+                                        activation="relu"),
+                          layers.MaxPooling2D(pool_size=2),
+                          layers.Conv2D(filters=64, kernel_size=3,
+                                        activation="relu"),
+                          layers.MaxPooling2D(pool_size=2),
+                          layers.Conv2D(filters=128, kernel_size=3,
+                                        activation="relu"),
+                          layers.MaxPooling2D(pool_size=2),
+                          layers.Conv2D(filters=256, kernel_size=3,
+                                        activation="relu"),
+                          layers.MaxPooling2D(pool_size=2),
+                          layers.Conv2D(filters=256, kernel_size=3,
+                                        activation="relu"),
+                          layers.Flatten(),
+                          layers.Dense(1, activation="sigmoid")])
+
+
+model.compile(loss="binary_crossentropy",
+              optimizer="rmsprop",
+              metrics=["accuracy"])
+
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath="model.keras",
+        save_best_only=True,
+        monitor="val_loss")]
+
+
+history = model.fit(
+    train_dataset,
+    epochs=30,
+    validation_data=validation_dataset,
+    callbacks=callbacks)
+
+
+accuracy = history.history["accuracy"]
+val_accuracy = history.history["val_accuracy"]
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+epochs = range(1, len(accuracy) + 1)
+plt.figure()
+plt.plot(epochs, accuracy, "bo", label="Training accuracy")
+plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
+plt.title("Training and validation accuracy")
+plt.legend()
+
+plt.figure()
+plt.plot(epochs, loss, "bo", label="Training loss")
+plt.plot(epochs, val_loss, "b", label="Validation loss")
+plt.title("Training and validation loss")
+plt.legend()
+plt.show()
+
+test_model = keras.models.load_model(
+    "convnet_from_scratch_with_augmentation.keras")
+test_loss, test_acc = test_model.evaluate(test_dataset)
+print(f"Test accuracy: {test_acc:.3f}")
