e6458fbccc7ce128a48c49b5b10a86967e720cb1
RamirezFernanda <ramirez.bmfernanda@gmail.com>
2022-11-09 15:32:12 -0600

    Add Module3

---
+++ Modulo3/Vader.py
@@ -0,0 +1,44 @@
+# import SentimentIntensityAnalyzer class
+# from vaderSentiment.vaderSentiment module.
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+# function to print sentiments
+# of the sentence.
+
+
+def sentiment_scores(sentence):
+
+    # Create a SentimentIntensityAnalyzer object.
+    sid_obj = SentimentIntensityAnalyzer()
+
+    # polarity_scores method of SentimentIntensityAnalyzer
+    # object gives a sentiment dictionary.
+    # which contains pos, neg, neu, and compound scores.
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+
+    print(end=" ")
+
+    # decide sentiment as positive, negative and neutral
+    if sentiment_dict['compound'] >= 0.05:
+        print("Positive")
+
+    elif sentiment_dict['compound'] <= - 0.05:
+        print("Negative")
+
+    else:
+        print("Neutral")
+
+
+def reading_dataset(dataset):
+    lst = []
+    f = open(dataset, "r")
+    for line in f:
+        lst.append(line.strip())
+    return lst
+
+
+# Driver code
+if __name__ == "__main__":
+    sentence = reading_dataset('DataSets\Tiny_movie_reviews_dataset.txt')
+    for i in sentence:
+        sentiment_scores(i)
