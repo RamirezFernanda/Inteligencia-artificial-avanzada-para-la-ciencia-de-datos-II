0527c4e0b7f9b6832458a28fab878ee4ed7e9031
Mikaela Grace <mikaelagrace@users.noreply.github.com>
2022-11-17 20:44:03 -0500

    Update vader.py
    No need to merge the Pull Request if you don't want to, it's just a way for me to suggest updates with inline diffs!

--- Module3/SentimentAnalysis/vader.py
+++ Module3/SentimentAnalysis/vader.py
@@ -6,17 +6,15 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 # function to print sentiments
 # of the sentence.
-def sentiment_scores(sentence):
+def get_sentiment_scores(sentence):
 
     # Create a SentimentIntensityAnalyzer object.
-    sid_obj = SentimentIntensityAnalyzer()
+    analyzer = SentimentIntensityAnalyzer()
 
     # polarity_scores method of SentimentIntensityAnalyzer
     # object gives a sentiment dictionary.
     # which contains pos, neg, neu, and compound scores.
-    sentiment_dict = sid_obj.polarity_scores(sentence)
-
-    print(end=" ")
+    sentiment_dict = analyzer.polarity_scores(sentence)
 
     # decide sentiment as positive, negative and neutral
     if sentiment_dict['compound'] >= 0.05:
@@ -31,9 +29,11 @@ def sentiment_scores(sentence):
 
 # Function that returns a list of strings that contains all the
 # sentences in a txt file
-def reading_dataset(dataset):
-    lst = []
+def read_dataset(dataset):  # function names should almost always start with verbs :) 
+    results = []
+    # you should use the "with" construction mentioned here: https://www.programiz.com/python-programming/file-operation
+    # so that the file is automatically closed. right now, you are leaving it open!
     f = open(dataset, "r")
     for line in f:
-        lst.append(line.strip())
-    return lst
+        results.append(line.strip())
+    return results
